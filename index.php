fa88c4897ec1a767bcf0bdf88b3fca7f87f32355
Willian Dias Brito <willian.dias360@hotmail.com>
2019-12-29 08:55:08 -0300

    Corrigindo as rotas

--- index.php
+++ index.php
@@ -8,11 +8,31 @@ require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->setBasePath('/apirestexample');
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
+$app->addBodyParsingMiddleware();
+$app->setBasePath("/apirestexample");
+
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write("Hello world!!!");
     return $response;
+});
+
+$app->post("/test", function (Request $request, Response $response, $args) {
+    $product = [
+        "CodigoProduto" => 1,
+        "Nome" => "Teste"
+    ];
+
+    $response->getBody()->write(json_encode($product));
+    return $response->withHeader('Content-Type', 'application/json')
+        ->withStatus(201);
+});
+
+$app->post('/books', function (Request $request, Response $response, $args) {
+    $valor = $request->getBody();
+    return $response->withBody($valor);
 });
 
 $app->run();
